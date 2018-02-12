444a1dda100ad6614364397a60413da12ec441ee
gmahechas <gmahechas@gmail.com>
2018-02-12 14:52:31 -0500

    Install
    graphql-laravel
    laravel-graphiql
    laravel-cors

--- config/app.php
+++ config/app.php
@@ -167,7 +167,9 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Rebing\GraphQL\GraphQLServiceProvider::class,
+        Graphiql\GraphiqlServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -225,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'GraphQL' => Rebing\GraphQL\Support\Facades\GraphQL::class,
 
     ],
 

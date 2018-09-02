868325321dc715ec921c2f2427df49b31b02949b
gmahechas <gmahechas@ingenieros.com>
2018-09-02 11:55:51 -0500

    add person_names in search person feature
    in web https://github.com/gmahechas/web/commit/c3e4fea75322ef64359a0a36c04298693f0e7e40

--- app/GraphQL/Query/Modules/Two/Person/PaginationPerson.php
+++ app/GraphQL/Query/Modules/Two/Person/PaginationPerson.php
@@ -29,6 +29,9 @@ class PaginationPerson extends Query
             'person_identification' => [
                 'type' => Type::string()
             ],
+            'person_names' => [
+                'type' => Type::string()
+            ],
             'limit' => [
                 'type' => Type::int()
             ],
@@ -45,6 +48,7 @@ class PaginationPerson extends Query
 
         $person_id = isset($args['person_id']) ? $args['person_id'] : false;
         $person_identification = isset($args['person_identification']) ? $args['person_identification'] : false;
+        $person_names = isset($args['person_names']) ? $args['person_names'] : false;
 
         $limit = isset($args['limit']) ? $args['limit'] : config('app.page_limit');
         $page = isset($args['page']) ? $args['page'] : 1;
@@ -55,6 +59,13 @@ class PaginationPerson extends Query
                         })
                         ->when($person_identification, function ($query) use ($person_identification) {
                             return $query->where('person_identification', '=', $person_identification);
+                        })
+                        ->when($person_names, function ($query) use ($person_names) {
+                            return $query->where('person_first_name', 'like', '%'.$person_names.'%')
+                                         ->orWhere('person_second_name', 'like', '%'.$person_names.'%')
+                                         ->orWhere('person_first_surname', 'like', '%'.$person_names.'%')
+                                         ->orWhere('person_second_surname', 'like', '%'.$person_names.'%')
+                                         ->orWhere('person_legal_name', 'like', '%'.$person_names.'%');
                         })
                         ->with($with)
                         ->paginate($limit, ['*'], 'pages', $page);
